ae5c746166f8f6448a9d7b38583bd55f031d8093
Yash Porwal <tanishporwal14dec@gmail.com>
2024-12-23 23:36:45 +0530

    Mongoose deployment changes

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ app.use(express.json());
 
 mongoose.connect(process.env.MONGO_URI, {useNewUrlParser:true, useUnifiedTopology:true})
 .then(()=> console.log('Connected to Mongoose!'))
-.catch((error) => console.log('Error Connecting to Mongooose' + error))
+.catch((error) => console.log('Error Connecting to Mongooose ATLAS' + error))
 
 
 app.get('/', (req, res) => {
