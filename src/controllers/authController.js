42684cb2efc0328bce2a44e1a8f1c45c4f21fa87
Yash Porwal <tanishporwal14dec@gmail.com>
2024-12-25 21:34:24 +0530

    adding debugger

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -82,6 +82,7 @@ const loginUser = async (req, res) => {
         }
 
         // Compare passwords
+        console.log(user.password)
         const passwordMatch = await bcrypt.compare(password, user.password);
         if (!passwordMatch) {
             return res.status(401).json({ 
