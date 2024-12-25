631b8c81cbd3515ee0caf97068f76fc04ad1277a
Yash Porwal <tanishporwal14dec@gmail.com>
2024-12-25 21:19:32 +0530

    fixing typo

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/userController');
-const authenticate = require('../middleware/authenticate');
-const authorize = require('../middleware/authorize');
+const authenticate = require('../middlewares/authenticate');
+const authorize = require('../middlewares/authorize');
 
 router.get('/', authenticate, authorize('Admin'), userController.getAllUsers);
 router.post('/add', authenticate, authorize('Admin'), userController.addUser);
