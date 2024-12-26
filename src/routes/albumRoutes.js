defadc55c7c77e201f33fde42ea8892b1bf1dba0
Yash Porwal <tanishporwal14dec@gmail.com>
2024-12-26 11:49:35 +0530

    add album authorization

--- src/routes/albumRoutes.js
+++ src/routes/albumRoutes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const albumController = require('../controllers/albumController');
 const authenticate = require('../middlewares/authenticate');
+const authorize = require('../middlewares/authorize')
 const router = express.Router();
 
 router.get('/', authenticate, albumController.getAllAlbums);
