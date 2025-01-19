cdc2495b213150de5aa7fe65aeec5cfc395e5635
Yash Porwal <tanishporwal14dec@gmail.com>
2025-01-19 15:06:50 +0530

    fixing error in function

--- src/config/redis.js
+++ src/config/redis.js
@@ -7,12 +7,29 @@ const client = redis.createClient({
 
 client.connect().catch(console.error);
 
-client.on('connect',() => {
+client.on('connect', () => {
     console.log('Redis Connected')
 })
 
-client.on('error',(error) => {
+client.on('error', (error) => {
     console.log('Error while connecting to Redis: ', error)
 })
+const setAsync = async (key, ttl, value) => {
+    try {
+        await client.setEx(key, ttl, value);
+        console.log(`Key "${key}" set with TTL ${ttl}`);
+    } catch (error) {
+        console.error('Error setting key in Redis:', error);
+    }
+};
 
-export default client;
+const delAsync = async (key) => {
+    try {
+        const result = await client.del(key);
+        console.log(`Key "${key}" deleted:`, result);
+    } catch (error) {
+        console.error('Error deleting key from Redis:', error);
+    }
+};
+
+export { setAsync, delAsync };
--- src/controllers/trackController.js
+++ src/controllers/trackController.js
@@ -3,10 +3,10 @@ const artistModel = require('../models/artistModel');
 const albumModel = require('../models/albumModel');
 const multer = require('multer');
 const path = require('path');
-const redisClient = require('../config/redis');
+const {setAsync, delAsync} = require('../config/redis');
 // const getAsync = promisify(client.get).bind(client);
-const setAsync = (key, ttl, value) => redisClient.setEx(key, ttl, value);
-const delAsync = (key) => redisClient.del(key);
+// const setAsync = (key, ttl, value) => redisClient.setEx(key, ttl, value);
+// const delAsync = (key) => redisClient.del(key);
 
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
