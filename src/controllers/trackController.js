cae92b1404de24a09fec0658ce5f16097ef1a826
Yash Porwal <tanishporwal14dec@gmail.com>
2025-01-19 11:44:45 +0530

    fixing error in function

--- src/controllers/trackController.js
+++ src/controllers/trackController.js
@@ -5,9 +5,8 @@ const multer = require('multer');
 const path = require('path');
 const redisClient = require('../config/redis');
 // const getAsync = promisify(client.get).bind(client);
-const { promisify } = require('util');
-const setAsync = promisify(redisClient.setEx).bind(redisClient);
-const delAsync = promisify(redisClient.del).bind(redisClient)
+const setAsync = (key, ttl, value) => redisClient.setEx(key, ttl, value);
+const delAsync = (key) => redisClient.del(key);
 
 
 const storage = multer.diskStorage({
